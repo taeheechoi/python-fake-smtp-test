24d65013f30e0484df2e0b3e5be08290cf106b5e
Tae Hee Choi <dadac76@hotmail.com>
2021-10-27 07:24:57 -0700

    initial commit

--- fake_email.py
+++ fake_email.py
@@ -1,19 +1,19 @@
 import smtplib
 
-sender = 'from@fromdomain.com'
-receivers = ['to@todomain.com']
+from_email = 'from_email@test.com'
+to_emails = ['to_email@test.com']
 
-message = '''From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
+message = '''From: From Person <from_email@test.com>
+To: To Person <to_email@test.com>
 Subject: SMTP e-mail test
 
-This is a test e-mail message.
+This is a fake e-mail message.
 '''
 
 if __name__ == '__main__':
   try:
     mail = smtplib.SMTP('127.0.0.1', 2525)
-    mail.sendmail(sender, receivers, message)         
+    mail.sendmail(from_email, to_emails, message)         
     print ('Successfully sent email')
   
   except:
